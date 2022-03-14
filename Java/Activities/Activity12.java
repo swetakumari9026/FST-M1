b1a03aacf30f551b7c56830a266793e7e04417e9
SWETA KUMARI <swetashaw9026@gmail.com>
2022-03-14 16:08:47 +0530

    Add files via upload

---
+++ Java/Activities/Activity12.java
@@ -0,0 +1,22 @@
+package com.company;
+
+@FunctionalInterface
+interface Addable{
+    int add(int num1,int num2);
+}
+public class Activity12 {
+
+    public static void main(String[] args) {
+        Addable ad1= (num1,num2)->num1+num2;
+
+        System.out.println(ad1.add(10, 20));
+
+        Addable ad2= (num1,num2)->{
+        return num1+num2;
+        };
+
+        System.out.println(ad2.add(100, 200));
+
+
+    }
+}
