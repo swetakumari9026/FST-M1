bacdd796b6611e53a97c76f82e874398632f96c5
SWETA KUMARI <swetashaw9026@gmail.com>
2022-03-19 17:53:16 +0530

    Add files via upload

---
+++ Python/Activities/Activity7.py
@@ -0,0 +1,10 @@
+list_of_number = []
+num = int(input('enter the size: '))
+for i in range(num):
+ array_of_number = int(input("enter number: "))
+ list_of_number.append(array_of_number)
+ print(list_of_number)
+ 
+ s  = sum(list_of_number)
+print(s)
+
