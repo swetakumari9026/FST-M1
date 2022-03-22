f1cc6d8363baf6e57955594a57c7997eeda73cb3
SWETA KUMARI <swetashaw9026@gmail.com>
2022-03-22 11:37:36 +0530

    Add files via upload

---
+++ Selenium/Activity1.java
@@ -0,0 +1,13 @@
+package com.ibm.Selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Activity1 {
+    public static void main(String[] args) {
+        WebDriver driver = new FirefoxDriver();
+        driver.get("https://google.com");
+        driver.close();
+
+    }
+}
