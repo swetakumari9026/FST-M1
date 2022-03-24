5af3fb55f37319ef7c071d4e13e9b977f279c331
SWETA KUMARI <swetashaw9026@gmail.com>
2022-03-24 12:13:07 +0530

    Add files via upload

---
+++ TestNG/Activities/Activity1.java
@@ -0,0 +1,44 @@
+package com.ibm.testng;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Activity1 {
+    WebDriver driver;
+
+    @BeforeClass
+    public void startBrowser(){
+        driver = new FirefoxDriver();
+        driver.get("https://training-support.net");
+    }
+
+    @Test
+    public void testTitle(){
+        // Check the title of the page
+        String title = driver.getTitle();
+
+        //Print the title of the page
+        System.out.println("Page title is: " + title);
+
+        //Assertion for page title
+        Assert.assertEquals("Training Support", title);
+
+        //Find the clickable link on the page and click it
+        driver.findElement(By.id("about-link")).click();
+
+        //Print title of new page
+        System.out.println("New page title is: " + driver.getTitle());
+
+        Assert.assertEquals(driver.getTitle(), "About Training Support");
+
+    }
+    @AfterClass
+    public void tearDown(){
+        driver.quit();
+    }
+}
